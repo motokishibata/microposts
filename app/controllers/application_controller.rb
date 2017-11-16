3b636b39d90e5b318472e0a278c8c3acdd09f871
motokishibata <shibata.youkan0701@gmail.com>
2017-11-16 13:58:09 +0000

    add post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,14 @@ class ApplicationController < ActionController::Base
   
   private
   
-  def require_user_logget_in
+  def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
+  
 end
